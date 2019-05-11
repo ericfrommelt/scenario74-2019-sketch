a0a4134ec6aec39b0ed9226a003c67a1e09a93e9
Eric Frommelt <ericfrommelt@gmail.com>
2019-05-11 14:45:24 -0400

    catching up

--- src/pages/winter19/index.js
+++ src/pages/winter19/index.js
@@ -1,9 +1,22 @@
-import React from "react" 
+import React from "react"
+import styles from "./index.module.css"
 import Masthead from "../../components/masthead"
 
 export default () => (
   <div>
     <Masthead></Masthead>
     <h1>Winter 2019</h1>
+
+    <section className={styles.threeByThree}>
+      <div className={styles.gridOne}></div>
+      <div className={styles.gridTwo}></div>
+      <div className={styles.gridThree}></div>
+      <div className={styles.gridFour}></div>
+      <div className={styles.gridFive}></div>
+      <div className={styles.gridSix}></div>
+      <div className={styles.gridSeven}></div>
+      <div className={styles.gridEight}></div>
+      <div className={styles.gridNine}></div>
+    </section>
   </div>
 )
