36696e0fa8639adb1de7cceda72c89635cbd608d
Eric Frommelt <ericfrommelt@gmail.com>
2019-05-11 15:18:03 -0400

    setup gatsby-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,19 @@
+const path = require(`path`)
+
 module.exports = {
   siteMetadata: {
     title: `SCENARIO74`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`
   ],
 }
