0179d016dece93f5ffd91c251d7f2e3819301293
Eric Frommelt <ericfrommelt@gmail.com>
2019-05-13 11:56:43 -0400

    setup web fonts

--- src/components/masthead.js
+++ src/components/masthead.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import mastheadStyles from "./masthead.module.css"
 import { StaticQuery, Link, graphql } from "gatsby"
 
@@ -15,9 +16,14 @@ export default ({ data }) => (
     `
   }
     render={data => (
-      <Link to={`/`}>
-        <h1 className={mastheadStyles.masthead}>{data.site.siteMetadata.title}</h1>
-      </Link>
+      <div>
+        <Helmet>
+          <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/7804816/7144992/css/fonts.css" />
+        </Helmet>
+        <Link to={`/`}>
+          <h1 className={mastheadStyles.masthead}>{data.site.siteMetadata.title}</h1>
+        </Link>
+      </div>
     )}
   />
 )
